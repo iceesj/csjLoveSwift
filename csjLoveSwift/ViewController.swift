6293098e21a2d1df3fc5d21cf58fbb4b34684462
csjwork <icsj@icloud.com>
2014-06-03 16:11:25 +0800

    test1

---
+++ csjLoveSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  csjLoveSwift
+//
+//  Created by iceesj on 14-6-3.
+//  Copyright (c) 2014年 iceesj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
