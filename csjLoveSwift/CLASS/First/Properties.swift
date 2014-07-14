a0be67b52758853ce115473339a03cfa9f2d665c
csjwork <icsj@icloud.com>
2014-07-14 17:29:31 +0800

    properties-1

--- csjLoveSwift/CLASS/First/Properties.swift
+++ csjLoveSwift/CLASS/First/Properties.swift
@@ -25,7 +25,43 @@ class Properties: CSJSwiftViewController {
     func propertiesMethod(){
         println("----属性----")
         
-        //存储属性
+        //存储属性，Stored Properties
+        //下面的例子定义了一个称为FixedLengthRange结构,描述了一个范围的整数范围长度,一旦创建不能改变:
+        struct FixedLengthRange{
+            var firstValue:Int
+            let length:Int
+        }
+        var rangeOfThreeItems = FixedLengthRange(firstValue:0, length: 3)
+        //the range represents integer values 0,1 and 2
+        rangeOfThreeItems.firstValue = 6
+        //the range now represents integer values 6,7 and 8
+        
+        //存储属性不变结构的实列，Stored Properties of Constant Structure Instances
+        //如果您创建一个结构的实例,并将该实例分配给一个常量,您不能修改实例的属性,即使他们被声明为变量属性:
+        let rangeOfFourItems = FixedLengthRange(firstValue: 0, length: 4)
+        //this range represents integer values 0,1,2 and 3
+//        rangeOfFourItems.firstValue = 6  //<---error
+        
+        //懒惰的存储属性，Lazy Stored Properties
+        class DataImporter{
+            var fileName = "data.text"
+        }
+        
+        /*
+        class DataManager {
+            @lazy var importer = DataImporter()
+            var data = [String]()
+        }
+        let manager = DataManager()
+        manager.data += "Some data"
+        manager.data += "Some more data"
+        println("manager.data = \(manager.data)")
+        println(manager.importer.fileName)
+        */
+        
+        //计算属性，Computed Properties
+        
+        
     }
 
     /*
